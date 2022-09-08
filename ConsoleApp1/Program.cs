63663d792dfca91c2f08be4d8fa66374f257b8e0
Sayeh Ghaderi <sayeh.ghaderi9@gmail.com>
2022-09-08 08:23:38 +0200

    Add the description

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace ConsoleApp1
+﻿using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
 {
     internal class Program
-    {
+    { //Exercise 1  
+       // Write a program and ask the user to enter a number.
+       // The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the
+       //console.Otherwise, display "Invalid".
         static void Main(string[] args)
         {
-            Console.Write("Enter a number:");
+            Console.Write("Enter a number like 3.5:");
             double number1 = Convert.ToDouble(Console.ReadLine());
            Console.WriteLine(number1);
             if (number1 <= 10 && number1 >= 1)
