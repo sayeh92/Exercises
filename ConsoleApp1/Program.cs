acbf15b3d5856c97a01cc02409edadfc98c860b2
deltagare <deltagare@DESKTOP-GCE7HLI>
2022-09-08 08:12:59 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+           Console.WriteLine(number1);
+            if (number1 <= 10 && number1 >= 1)
+            {
+                Console.WriteLine("valid");
+            } else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
